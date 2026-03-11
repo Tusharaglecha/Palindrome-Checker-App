db713467087f4347ec379610df602204029e078d
Tushar <tusharaglecha15@gmail.com>
2026-03-11 08:26:14 +0530

    Main.java
    UC1: Application Entry & Welcome Message

--- src/UC1/Main.java
+++ src/UC1/Main.java
@@ -2,22 +2,17 @@ package UC1;
 
 public class Main {
 
-    /**
-     * Application entry point.
-     * This method is executed by the JVM when the program starts.
-     */
+
     public static void main(String[] args) {
 
-        // Welcome Message
         System.out.println("======================================");
         System.out.println("      PALINDROME CHECKER APP");
         System.out.println("======================================");
 
-        // Application Information
         System.out.println("Application Name : Palindrome Checker App");
         System.out.println("Version          : 1.0");
 
-        // Startup Message
+   
         System.out.println("--------------------------------------");
         System.out.println("Application started successfully.");
 
